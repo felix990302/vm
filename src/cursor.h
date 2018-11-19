831071286f0ba488b9fe4a8895036987ccc0020d
fzhou <felix990302@yahoo.ca>
2018-11-19 16:48:14 -0500

    wrote cursor header

--- src/cursor.h
+++ src/cursor.h
@@ -2,30 +2,12 @@
 #define CURSOR_H
 
 #include <cstddef>
-#include <utility>
 
 
 namespace VM {
-    template<typename T, typename U> class Cursor {
-        typedef T lineMarker;
-        typedef U charMarker;
-        typedef std::pair<lineMarker, charMarker> position;
-
-        position posn;
-
-        public:
-        Cursor(const lineMarker &linePosn, const charMarker &charPosn):
-            posn{linePosn, charPosn}
-        {}
-        const position &getPosn() const {return posn;}
-        const lineMarker &setLinePosn() const {return posn.first;}
-        const charMarker & getCharPosn() const {return posn.second;}
-        void setLinePosn(const lineMarker &newLinePosn) {posn.first = newLinePosn;}
-        void setCharPosn(const charMarker &newCharPosn) {posn.second = newCharPosn;}
-        void setPosn(const size_t &newLinePosn, const size_t &newCharPosn) {
-            setLinePosn(newLinePosn);
-            setCharPosn(newCharPosn);
-        }
+    struct Cursor {
+        size_t line;
+        size_t col;
     };
 }
 
