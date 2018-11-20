a5726578123dbeec86f4c32ec1124ccaedcf749a
fzhou <felix990302@yahoo.ca>
2018-11-20 01:57:58 -0500

    added direction, insert, delete command headers

---
+++ src/controller/direction.h
@@ -0,0 +1,14 @@
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+
+namespace VM {
+    enum class Direction {
+        Up,
+        Down,
+        Forward,
+        Backward,
+    };
+}
+
+#endif
---
+++ src/controller/insert_command.h
@@ -0,0 +1,22 @@
+#ifndef INSERT_COMMAND_H
+#define INSERT_COMMAND_H
+
+#include <string>
+#include "command.h"
+#include "direction.h"
+
+
+namespace VM {
+    class InsertCommand: Command {
+        const Direction direction;
+        std::string toInsert;
+
+        public:
+        void doCommand() override;
+        std::unique_ptr<Command> clone() override;
+
+        InsertCommand(const size_t &quant, Direction direction, const std::string &toInsert);
+    };
+}
+
+#endif
