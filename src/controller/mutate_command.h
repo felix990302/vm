abaaf9d07270073bf57be954ad0086252e875f89
fzhou <felix990302@yahoo.ca>
2018-11-20 19:43:09 -0500

    fixed inheritance hierarchy for insert and delete commands

---
+++ src/controller/mutate_command.h
@@ -0,0 +1,19 @@
+#ifndef MUTATE_COMMAND_H
+#define MUTATE_COMMAND_H
+
+#include <string>
+#include "undoable_command.h"
+#include "direction.h"
+
+
+namespace VM {
+    class MutateCommand: public UndoableCommand {
+        const Direction direction;
+        std::string toMutate;
+
+        public:
+        MutateCommand(const size_t &quant, Direction direction, const std::string &toMutate);
+    };
+}
+
+#endif
