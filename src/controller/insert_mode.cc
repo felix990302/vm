7c7e01f6d608e88a33da26173890379f05bf0686
felix990302 <felix990302@yahoo.ca>
2018-11-25 02:42:09 -0500

    suppots arrow keys for insert mode

--- src/controller/insert_mode.cc
+++ src/controller/insert_mode.cc
@@ -1,5 +1,6 @@
 #include <ncurses.h>
 #include "model/file_buffer.h"
+#include "controller.h"
 #include "insert_mode.h"
 #include "command/mutate_command/insert_newline_command.h"
 
@@ -8,6 +9,24 @@
 namespace VM {
     bool InsertMode::processChar(int c) {
         switch(c) {
+            case KEY_UP: {
+                controller.getBuffer()->ptrCursor.moveUp();
+                controller.getBuffer()->ptrCursor.moveRight(0); // FIXME: proper way to garantee cursor is valid position
+                break;
+            }
+            case KEY_DOWN: {
+                controller.getBuffer()->ptrCursor.moveDown();
+                controller.getBuffer()->ptrCursor.moveRight(0); // FIXME: proper way to garantee cursor is valid position
+                break;
+            }
+            case KEY_LEFT: {
+                controller.getBuffer()->ptrCursor.moveLeft();
+                break;
+            }
+            case KEY_RIGHT: {
+                controller.getBuffer()->ptrCursor.moveRight();
+                break;
+            }
             case '\n': {
                 std::unique_ptr<Command> newline = std::make_unique<InsertNewlineCommand> (1);
                 newline->doCommand(controller);
