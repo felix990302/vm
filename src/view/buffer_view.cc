efce5379a1b6238135402b4ee992c9cade08503d
fzhou <felix990302@yahoo.ca>
2018-11-25 03:46:15 -0500

    Revert "added note about clearing to end of line for deltes" as already fixed
    This reverts commit 1a81d0c4c3d4efcc1783e85175250d91b9a9c7d6.

--- src/view/buffer_view.cc
+++ src/view/buffer_view.cc
@@ -4,7 +4,7 @@
 #include <iostream>
 
 namespace VM{
-    void BufferView::draw(const Coordinates &parentPosn, TextDisplay &display) // FIXME: deletion would need to clear to end of line somehow
+    void BufferView::draw(const Coordinates &parentPosn, TextDisplay &display)
     {
         size_t newCursorY = cursor.line;
         size_t newCursorX = cursor.col;
