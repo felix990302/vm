5f604b487daac2bd96287f4265585e45310dbb46
felix990302 <felix990302@yahoo.ca>
2018-11-24 21:44:14 -0500

    added start position to interface for motion

--- src/controller/command/motion.h
+++ src/controller/command/motion.h
@@ -8,6 +8,7 @@ class Cursor;
 
 namespace VM {
     struct Motion {
+        virtual Cursor startPosition(const Cursor &cursor) = 0;
         virtual Cursor nextPosition(const Cursor &cursor) = 0;
         virtual std::unique_ptr<Motion> clone() = 0;
     };
