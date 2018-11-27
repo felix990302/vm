8e9c0080bf830dcc4124158d9222763c1ab6bb64
felix990302 <felix990302@yahoo.ca>
2018-11-26 23:30:02 -0500

    almost fully implemented colon commands

--- src/controller/mode/lazy_command_mode/colon_command_mode.cc
+++ src/controller/mode/lazy_command_mode/colon_command_mode.cc
@@ -1,23 +1,55 @@
 #include <cstdlib>
 #include "utility/trim.h"
+#include "model/file_buffer.h"
+#include "model/write_exception.h"
+#include "controller/controller.h"
+#include "controller/quit_exception.h"
 #include "controller/mode/parsing_exception.h"
+#include "controller/command/write_command.h"
+#include "controller/command/quit_command.h"
+#include "controller/command/move_command.h"
+#include "controller/command/motion/absolute_motion.h"
 #include "colon_command_mode.h"
 
 
 namespace VM {
     std::unique_ptr<Command> ColonCommandMode::parseCommand(const std::string &command, const std::string &argument) {
-        if(command == "wq") {}
-        else if(command == "w") {}
+        if(command == "wq") {
+            if(argument.empty()) {
+                return std::make_unique<WriteCommand>(true);
+            }
+            else {
+                return std::make_unique<WriteCommand>(true, argument);
+            }
+        }
+        else if(command == "w") {
+            if(argument.empty()) {
+                return std::make_unique<WriteCommand>(false);
+            }
+            else {
+                return std::make_unique<WriteCommand>(false, argument);
+            }
+        }
         else if(command == "r") {}
         else if(argument.empty()) {
-            if(command == "q") {}
-            else if(command == "q!") {}
-            else if(command == "$") {}
-            else if(command == "0") {}
+            if(command == "q") {
+                return std::make_unique<QuitCommand>(false);
+            }
+            else if(command == "q!") {
+                return std::make_unique<QuitCommand>(true);
+            }
+            else if(command == "$") {
+                return std::make_unique<MoveCommand>(1, std::make_unique<AbsoluteMotion>(controller.getBuffer().getBuffer().size()-1));
+            }
+            else if(command == "0") {
+                return std::make_unique<MoveCommand>(1, std::make_unique<AbsoluteMotion>(0));
+            }
             else {
                 char *end;
                 long int lineTarget = strtol(command.c_str(), &end, 10);
-                if(lineTarget && *end=='\0') {}
+                if(lineTarget && *end=='\0') {
+                    return std::make_unique<MoveCommand>(1, std::make_unique<AbsoluteMotion>(lineTarget));
+                }
                 else {
                     throw InvalidCommandException {};
                 }
@@ -39,5 +71,7 @@ namespace VM {
             theCommand->doCommand(controller);
         }
         catch(const InvalidCommandException&) {}
+        catch(const FileWriteException&) {}
+        catch(const ProgramQuitException&) {}
     }
 }
